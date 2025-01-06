ae07cef70b635e41824f35ff8215ac4eb8575914
DevShaded <devshaded@devshaded.com>
2025-01-06 15:07:40 +0100

    feat: publish cors & api

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/', function () {
+    // return json response with status code 200 and the version of the project
+    return response()->json([
+        'message' => 'Dette er API\'en til Aka24. Alle forespørsler må sendes til /api/*',
+        'version' =>  '1.0.0'
+    ], 200);
+});
