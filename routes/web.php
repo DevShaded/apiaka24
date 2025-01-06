62e99c5f4101386b7f1eae8eac2cbbf29c4b0538
DevShaded <devshaded@devshaded.com>
2025-01-06 14:47:27 +0100

    feat: api response on home page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return json response with status code 200 and the version of the project
+    return response()->json([
+        'message' => 'Dette er API\'en til Aka24. Alle forespørsler må sendes til /api/*',
+        'version' =>  '1.0.0'
+    ], 200);
 });
