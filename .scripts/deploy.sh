901f536f932d89cbe6d906e718e37e01fcca707d
DevShaded <devshaded@devshaded.com>
2025-01-10 11:29:27 +0100

    feat: deploy script

---
+++ .scripts/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+# Pull the latest version of the app
+git pull origin main
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Update Composer
+composer update
+
+# Filament Upgrade
+php artisan filament:upgrade
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+php artisan filament:optimize
+
+# Compile npm assets
+npm run prod
+
+# Run database migrations
+php artisan migrate --force
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
